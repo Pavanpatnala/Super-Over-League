a953896bd77462241fa6f6281fa927604c5537e0
pavan <pavankumarpatnala96@gmail.com>
2023-04-11 05:43:23 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="bgContainer">
+    <h1 className="heading">Super Over League</h1>
+    <div className="imgContainer">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
